0dda3d8b4044ab36cc4cfc5f2dc83611acaa9295
Blaiteray <66131047+Blaiteray@users.noreply.github.com>
2021-10-02 01:27:39 +0600

    adding files

---
+++ src/template.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FASTio ios::sync_with_stdio(false);cin.tie(NULL);
+#define DECI fixed<<setprecision(5)
+// #include <ext/pb_ds/assoc_container.hpp> 
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+// typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> indexed_set;
+// typedef tree<int,null_type,less_equal<int>,rb_tree_tag,tree_order_statistics_node_update> indexed_multiset;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef pair<int,int> pii;
+typedef priority_queue<int> pqi;
+typedef deque<int> di;
+#define pb(k) push_back(k)
+#define mp(a,b) make_pair(a,b)
+#define B begin();
+#define E end();
+#define nl cout<<"\n"
+#define DB(x) {static int testInt=1000;if((testInt--)>0)cout<<"(LINE "<<__LINE__<<": VALUE "<<x<<")\t";}
+#define LB {static int testIntx=0;if(testIntx<1000)cout<<"(LINE "<<__LINE__<<","<<testIntx+1<<")\t";else break;testIntx++;}
+#define TA(arr) {int* lLe=(int*)(&arr+1);for(int* xTe=arr;xTe!=lLe;xTe++) cout<<*xTe<<" ";nl;}
+#define nax 1000000007
+/**********************************/
+
+int main() {
+	FASTio
+	int t; cin >> t; while(t--) {
+		LB
+	}
+	return 0;
+}
